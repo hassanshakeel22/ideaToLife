0706af51b0e5303a7ae0f66efadccd8b44d38e38
hassanshakeel22 <155888423+hassanshakeel22@users.noreply.github.com>
2024-01-07 22:37:52 +0500

    Delete assignment directory

--- assignment/src/main/java/com/java/assignment/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.java.assignment.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java.assignment.model.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-
-}
